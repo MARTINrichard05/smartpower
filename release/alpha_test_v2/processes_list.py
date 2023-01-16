4c48d8b9fd110af6c1f4f246753fa09838db308f
richou <richou672005@protonmail.com>
2023-01-16 09:10:47 +0100

    v2 fixed

--- release/alpha_test_v2/processes_list.py
+++ release/alpha_test_v2/processes_list.py
@@ -1,11 +1,19 @@
 appstdp = {
     '/bin/bash --rcfile /home/richou/.local/share/JetBrains/Toolbox/apps/PyCharm-P/ch-0/223.7571.203/plugins/terminal/jediterm-bash.in -i' : 15000,
     'C:\\Program Files\\Rockstar Games\\Launcher\\Launcher.exe' : 25000,
-    '/usr/lib/brave-browser/brave' : 9000
+    '/usr/lib/brave-browser/brave' : 9000,
+    '/usr/lib/systemd/systemd-journald' : 8000,
+    'bash /home/richou/.local/share/Steam/steam.sh' : 25000,
+    '/usr/lib/libreoffice/program/soffice.bin --writer --splash-pipe=5' : 9000,
+    '/app/extra/bin/lunarclient --type=zygote --no-zygote-sandbox --no-sandbox' : 15000
 }
 
 appstmp = {
     '/bin/bash --rcfile /home/richou/.local/share/JetBrains/Toolbox/apps/PyCharm-P/ch-0/223.7571.203/plugins/terminal/jediterm-bash.in -i' : 70,
-    'C:\\Program Files\\Rockstar Games\\Launcher\\Launcher.exe' : 74,
-    '/usr/lib/brave-browser/brave' : 68
+    'C:\\Program Files\\Rockstar Games\\Launcher\\Launcher.exe' : 80,
+    '/usr/lib/brave-browser/brave' : 68,
+    '/usr/lib/systemd/systemd-journald' : 65,
+    'bash /home/richou/.local/share/Steam/steam.sh' : 74,
+    '/usr/lib/libreoffice/program/soffice.bin --writer --splash-pipe=5' : 66,
+    '/app/extra/bin/lunarclient --type=zygote --no-zygote-sandbox --no-sandbox' : 74
 }
