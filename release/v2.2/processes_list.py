ed84cee3b6232c9d1b8f9d30436758a8d9df101f
richou <richou672005@protonmail.com>
2023-01-17 22:10:24 +0100

    revrote the 'config saving' system, now it uses a json file, easy to read and write + faster

---
+++ release/v2.2/processes_list.py
@@ -0,0 +1,13 @@
+appstdp = {
+    '/bin/bash --rcfile /home/richou/.local/share/JetBrains/Toolbox/apps/PyCharm-P/ch-0/223.7571.203/plugins/terminal/jediterm-bash.in -i' : 15000,
+    'C:\\Program Files\\Rockstar Games\\Launcher\\Launcher.exe' : 25000,
+    '/usr/lib/brave-browser/brave' : 10000,
+    '/usr/lib/systemd/systemd-machined' : 10000
+}
+
+appstmp = {
+    '/bin/bash --rcfile /home/richou/.local/share/JetBrains/Toolbox/apps/PyCharm-P/ch-0/223.7571.203/plugins/terminal/jediterm-bash.in -i' : 73,
+    'C:\\Program Files\\Rockstar Games\\Launcher\\Launcher.exe' : 80,
+    '/usr/lib/brave-browser/brave' : 70,
+    '/usr/lib/systemd/systemd-machined' : 70
+}
---
+++ release/v2.2/tui.py
@@ -0,0 +1,37 @@
+from multiprocessing.connection import Client
+
+address = ('localhost', 6000)
+#conn = Client(address, authkey=b'eogn68rb8r69')
+# can also send arbitrary objects:
+# conn.send(['a', 2.5, None, int, sum])
+#conn.close()
+class Main:
+    def __init__(self):
+        self.conn = Client(address, authkey=b'eogn68rb8r69')
+        self.main()
+    def send(self, data):
+        try:
+            self.conn.send(data)
+        except :
+            pass
+    def main(self):
+        while True:
+            command = input('>>> ').split(' ')
+            if command[0] == 'help':
+                print('A way to interact without root privileges with my smartpower daemon')
+                print(' list of commands available:')
+                print('mode : use mode + arg to set it')
+                print('       turbo : turn all up to get maximum power (all *ratio , up to the cap)')
+                print('       normal : working normally as in config files and the process list')
+                print('       eco    : turn tdp down by the wanted ratio but keep temps normals')
+                print('       turn down max temp by the wanted ratio to be silent')
+                print('ratio : set the desired ratio for modes:')
+                print('       syntax : [mode] [ratio]')
+            if command[0] == 'mode':
+                self.send(command[0] + ' ' + command[1])
+            if command[0] == 'ratio':
+                self.send(command[0] + ' ' + command[1] + ' ' + command[2])
+
+if __name__ == '__main__':
+    a = Main()
+
